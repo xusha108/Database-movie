954c579028f2611d2f560763b7c03c856015745b
unknown <xusha108@gmail.com>
2020-07-31 12:59:14 +0300

    dinamic background

--- src/components/Movies/MovieCard.js
+++ src/components/Movies/MovieCard.js
@@ -7,34 +7,31 @@ import '../Movies/MovieCard.scss';
 class MovieCard extends Component {
 
   componentDidMount() {
-    this.props.get();
+    this.props.get(this.props.match.params.id);
   } 
 
-  render() {
-    const{movies} = this.props 
-    let htmlGenres = '';
-    let htmlProduction = '';
-    if(typeof movies.genres === 'object'){
-      htmlGenres = movies.genres.map((item, index) => {
-        return <span key={index}>{item.name}{ movies.genres.length - 1 === item ? '' : ', '}</span>
-      })
 
-      htmlProduction=movies.production_countries.map((item, index) => {
-        return <span key={index}>{item.name}{ movies.production_countries.length - 1 === item ? '' : ', '}</span>
-      })
+  render() {
+    const {movies} = this.props    
+    
+    let styles = {
+      backgroundImage:  `url(https://image.tmdb.org/t/p/original/${this.props.movies.backdrop_path})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      //filter: "brightness(40%)"      
     }
-    console.log("movies", movies)    
+    
     return (
-      <div className="movie-card">
-       <img src={`https://image.tmdb.org/t/p/original${movies.poster_path}`} alt='' />
+      <div className="movie-card" style={styles}> 
+        <div className='movie-card-poster'>     
+          <img src={`https://image.tmdb.org/t/p/original${movies.poster_path}`} alt='' />
+       </div>  
 
        <div className="movie-card-info">
         <h1> {movies.title}</h1>
         <q className="movie-card-info-tagline"> {movies.tagline}</q>
         <h3 className="movie-card-info-overview">Overview: {movies.overview}</h3>
-        <p className="movie-card-info-popularity">Popularity: {movies.popularity}</p>       
-        <p>Genres: {htmlGenres}</p>
-        <p>Countries: {htmlProduction}</p>
+        <p className="movie-card-info-popularity">Popularity: {movies.popularity}</p> 
         <p className="movie-card-info-release_date">Date of release: {movies.release_date}</p> 
        </div>
       </div>
@@ -49,7 +46,7 @@ const mapStateToProps = (store) => {
 
 const mapDispatchToProps = (dispatch) => { 
   return {
-    get: () => { getMovieById(dispatch) }
+    get: (id) => { getMovieById(id)(dispatch) } //
   }
 };
 
--- src/redux/actions/getMovieById.js
+++ src/redux/actions/getMovieById.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-export function getMovieById (dispatch) {
+export const getMovieById = (id) => dispatch => { 
+ 
   axios
-    .get('https://api.themoviedb.org/3/movie/157336?api_key=3d2f97884c550b7e4e4bee8b99c164c1&language=en-US')
+    .get(`https://api.themoviedb.org/3/movie/${id}?api_key=3d2f97884c550b7e4e4bee8b99c164c1&language=en-US`)
     .then(res => {
       const data = res.data
       console.log(data)
